476f3ae48f0ba063392c15779b39131d66e15623
richardcalahan <richard@calahan.me>
2013-11-04 00:26:37 -0500

    better internal DSL

--- lib/mongoid/lists.rb
+++ lib/mongoid/lists.rb
@@ -6,12 +6,6 @@ module Mongoid
 
     extend ActiveSupport::Concern
 
-    FIELD_SUFFIX = '_position'    
-
-    include do 
-      after_add 
-    end
-
     module ClassMethods
 
       # Macro to set relation on which to make a list
@@ -24,27 +18,32 @@ module Mongoid
       # @since 0.0.1
       def lists relation, options={}
         meta       = reflect_on_association relation
-        field_name = options[:column] || (meta.foreign_key.to_s.gsub(/_?id$/, FIELD_SUFFIX)).to_sym
-        klass      = meta.klass        
+        field_name = options[:column] || 
+          (meta.foreign_key.to_s.gsub(/_?id$/, '_position')).to_sym
+        
 
-        ids_setter_name = "#{relation.to_s.singularize}_ids="
-        ids_setter      = instance_method ids_setter_name
-        re_define_method ids_setter_name do |ids|
-          # assign new position int
+        # Override model#{name}_ids=        
+        before_method self, "#{relation.to_s.singularize}_ids=" do |ids|
           ids.each_with_index do |id, index|
-            klass.find(id).update_attribute field_name, index + 1
+            meta.klass.find(id).update_attribute field_name, index + 1
           end
 
-          # unassign old position ints
-          klass.where(meta.foreign_key => id).not_in(id: ids).each do |obj|
+          meta.klass.where(meta.foreign_key => id).not_in(id: ids).each do |obj|
             obj.update_attribute field_name, nil
           end
-
-          # invoke original method
-          ids_setter.bind(self).call(ids)
         end
 
         meta[:order] = "#{field_name} asc"
+      end
+
+      private
+
+      def before_method owner, method, &block
+        original_method = instance_method method
+        owner.re_define_method method do |*args|
+          block.call *args
+          original_method.bind(self).call *args
+        end
       end
 
     end
