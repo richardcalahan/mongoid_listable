5a0c7c86d37d70540d35f91d0d172e05ff8659f9
richardcalahan <richard@calahan.me>
2013-11-09 13:20:57 -0500

    testing removing photo from user

--- spec/mongoid_listable_spec.rb
+++ spec/mongoid_listable_spec.rb
@@ -2,29 +2,34 @@ require 'spec_helper'
 
 describe Mongoid::Listable do 
 
-  before do 
+  before :all do 
     @user = User.create!
-  end
+    @photos = []
+    10.times { @photos << Photo.create }
+  end  
 
   it 'created a user' do
     expect(@user).to be_true
   end
 
-  it 'created 10 photos' do
-    photos = []
-    10.times { photos << Photo.create }
-    expect(photos.count).to eq(10)
+  it 'created 10 photos' do    
+    expect(@photos.count).to eq(10)
   end
 
   it 'assigns all photos to a user with the default setter' do 
     @user.photos = Photo.all
-    @user.save
+    expect(@user.photos.count).to eq(@photos.count)
   end
 
   it 'assigns all photos to a user with the default ids setter' do 
     ids = Photo.all.pluck :id
     @user.photo_ids = ids
-    @user.save
+    expect(@user.photos.count).to eq(@photos.count)
+  end
+
+  it 'removes a photo from a user' do 
+    @user.photos.delete Photo.first
+    expect(@user.photos.count).to eq(@photos.count - 1)    
   end
 
 end
