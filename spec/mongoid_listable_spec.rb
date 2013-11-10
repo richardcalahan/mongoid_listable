7d178e0a290e71871e31811b1df8bfdbada1487e
richardcalahan <richard@calahan.me>
2013-11-10 17:09:07 -0500

    better test coverage

--- spec/mongoid_listable_spec.rb
+++ spec/mongoid_listable_spec.rb
@@ -90,19 +90,62 @@ describe Mongoid::Listable do
       ensure_order User.first.photos, :user_position
     end
 
-    it 'sets object list of an owner with the default setter' do 
-      User.first.photos = Photo.all
+    it 'sets object list of an owner with the default setter' do
+      photos = Photo.all
+      User.first.photos = photos
+      photos.each_with_index do |photo, index|
+        expect(photo.id).to eq(User.first.photos[index].id)
+      end
+      expect(User.first.photos.count).to eq(Photo.count)
     end
 
     it 'sets object list of an owner with the default ids setter' do 
       ids = Photo.all[2..7].collect(&:id)
       User.first.photo_ids = ids
+      ids.each_with_index do |id, index|
+        expect(id).to eq(User.first.photos[index].id)
+      end
       expect(User.first.photos.count).to eq(6)
     end
 
     it 'pushes objects to the list of an owner' do 
       User.first.photos << Photo.all
       expect(User.first.photos.count).to eq(10)
+    end
+
+    it 'updates the position of an object higher' do 
+      photo_id = User.first.photos[4].id
+      Photo.find(photo_id).update_attribute :user_position, 6
+      expect(photo_id).to eq(User.first.photos[5].id)
+      expect(User.first.photos[5].user_position).to eq(6)
+    end
+
+    it 'updates the position of an object lower' do 
+      photo_id = User.first.photos[4].id
+      Photo.find(photo_id).update_attribute :user_position, 1
+      expect(photo_id).to eq(User.first.photos[0].id)
+      expect(User.first.photos[0].user_position).to eq(1)
+    end
+
+    it 'updates the position of an object the same' do 
+      photo_id = User.first.photos[4].id
+      Photo.find(photo_id).update_attribute :user_position, 5
+      expect(photo_id).to eq(User.first.photos[4].id)
+      expect(User.first.photos[4].user_position).to eq(5)
+    end
+
+    it 'updates the position of an object out of bounds high' do 
+      photo_id = User.first.photos[4].id
+      Photo.find(photo_id).update_attribute :user_position, 1000
+      expect(photo_id).to eq(User.first.photos.last.id)
+      expect(User.first.photos.last.user_position).to eq(User.first.photos.count)
+    end
+
+    it 'updates the position of an object out of bounds low' do 
+      photo_id = User.first.photos[4].id
+      Photo.find(photo_id).update_attribute :user_position, -2
+      expect(photo_id).to eq(User.first.photos.first.id)
+      expect(User.first.photos.first.user_position).to eq(1)
     end
 
     it 'removes objects from the list of an owner with the default unsetter' do 
